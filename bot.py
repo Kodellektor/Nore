2f8f2628f5cc0e77590fbce5393f231501c9545d
Arturo Torres Jimenez <a7torres@ucsd.edu>
2024-04-05 06:28:14 -0700

    sample cogs and cog loading setup

--- bot.py
+++ bot.py
@@ -3,9 +3,32 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+#load environment variables and establish client 
 load_dotenv()
 current_channel = int(os.getenv('CHANNEL_ID_NUMBER'))
+client = commands.Bot(command_prefix = '$', help_command=None, intents = discord.Intents.all())
 
-client = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
+#load cogs
+cogs_directory = os.getenv('COGS_DIRECTORY')
+COGS = []
+with os.scandir(cogs_directory) as cogsdir:
+    for entry in cogsdir:
+        COGS.append(entry)
+cogsdir.close()
+for cog in COGS:
+    client.load_extension(f'cog.{cog}')
 
-client.run(os.getenv('BOT_TOKEN'))
+#commands/actions called when bot is fully ready
+@client.event
+async def on_ready():
+    """
+    This function listens for the event of fully loaded
+    bot, which is different from start-up. 
+    """
+    
+    print(f'{client.user} has logged in!')
+    
+    welcome = await client.fetch_channel(current_channel)
+    await welcome.send('Let your fortunes flow!')
+
+client.run(os.getenv('AUTHENTICATION_TOKEN')) #establish client connection 
